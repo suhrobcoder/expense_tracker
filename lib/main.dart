8cd1bbb9a6f698533085019616e3e85b19e94a07
suhrobcoder <suhrobcoder@gmail.com>
2023-05-02 21:41:05 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:expense_tracker/di/init_get_it.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Text('Expense Tracker'),
+    );
+  }
+}
