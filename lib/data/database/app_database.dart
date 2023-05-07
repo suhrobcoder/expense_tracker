0a09794cf4be83a893b4a29a63d304da5b38297e
suhrobcoder <suhrobcoder@gmail.com>
2023-05-07 11:15:33 +0500

    database setup

---
+++ lib/data/database/app_database.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:injectable/injectable.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+part 'app_database.g.dart';
+
+const _dbVersion = 1;
+const _dbName = "app.db";
+
+@singleton
+@DriftDatabase(
+  tables: [],
+)
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => _dbVersion;
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      beforeOpen: (details) async {
+        await customStatement('PRAGMA foreign_keys = ON');
+      },
+    );
+  }
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, _dbName));
+    return NativeDatabase.createInBackground(file);
+  });
+}
--- lib/di/init_get_it.config.dart
+++ lib/di/init_get_it.config.dart
@@ -9,8 +9,9 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:expense_tracker/data/database/app_database.dart' as _i3;
 import 'package:expense_tracker/presentation/pages/main/bloc/main_bloc.dart'
-    as _i3;
+    as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -25,7 +26,8 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.MainBloc>(() => _i3.MainBloc());
+    gh.singleton<_i3.AppDatabase>(_i3.AppDatabase());
+    gh.factory<_i4.MainBloc>(() => _i4.MainBloc());
     return this;
   }
 }
